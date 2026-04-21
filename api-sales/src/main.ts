688a5073bfa11d27b0150fe5a2c15292cbac6a7c
Laszlo-Caballero <laszlocaballero@gmail.com>
2026-04-20 20:02:08 -0500

    feat: initialize new NestJS project in api-sales directory

---
+++ api-sales/src/main.ts
@@ -0,0 +1,32 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Transport, MicroserviceOptions } from '@nestjs/microservices';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.REDIS,
+      options: {
+        host: process.env.REDIS_HOST,
+        port: parseInt(process.env.REDIS_PORT!),
+      },
+    },
+  );
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  await app.listen();
+
+  console.log(
+    `Sales microservice is listening on port ${process.env.REDIS_PORT}`,
+  );
+}
+bootstrap();
